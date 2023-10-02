927cb52fe9cc7a1aa0816f926b6ab42acf3e9b05
Melissa Armstrong <mkarmstr@gmail.com>
2023-10-01 23:19:27 -0400

    make toast

---
+++ src/components/ToastShelf/ToastShelf.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { ToastContext } from '../ToastProvider';
+import Toast from '../Toast';
+import styles from './ToastShelf.module.css';
+
+function ToastShelf() {
+  const { toasts } = React.useContext(ToastContext);
+
+  return (
+    <ol className={styles.wrapper}>
+      {toasts.map((toast) => (
+        <li key={toast.id} className={styles.toastWrapper}>
+          <Toast id={toast.id} variant={toast.variant}>
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default ToastShelf;
